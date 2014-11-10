84131c46d8526662f33c598b85045e9f40173ef1
HuangShuhang_Y470 <yhnbgfd@vip.qq.com>
2014-11-11 00:19:59 +0800

    fix：Model未实例化的BUG

--- LuckDraw/Models/WowRole/Model_WowRole.cs
+++ LuckDraw/Models/WowRole/Model_WowRole.cs
@@ -1,14 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace LuckDraw.Models.WowRole
+﻿namespace LuckDraw.Models.WowRole
 {
     class Model_WowRole
     {
-        public string Occupation { get; set; }
-        public string Race { get; set; }
-        public string Gender { get; set; }
+        private string _occupation = "";
+
+        public string Occupation
+        {
+            get { return _occupation; }
+            set { _occupation = value; }
+        }
+        private string _race = "";
+
+        public string Race
+        {
+            get { return _race; }
+            set { _race = value; }
+        }
+        private string _gender = "";
+
+        public string Gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+
+
+
     }
 }
